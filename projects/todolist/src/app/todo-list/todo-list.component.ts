4296f314e853f7dc9a3463bf42ba2173015ea945
younesmimoun <105279680+younesmimoun@users.noreply.github.com>
2024-01-12 09:44:12 +0100

    Résolu les conflits de fusion

--- projects/todolist/src/app/todo-list/todo-list.component.ts
+++ projects/todolist/src/app/todo-list/todo-list.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {BorderHighlightDirective} from "../border-highlight.directive";
 import {TODOS} from "../mock-todo";
-import {Todo} from "../todo";
 import {TodoComponent} from "../todo/todo.component";
 
 @Component({
@@ -13,11 +12,11 @@ import {TodoComponent} from "../todo/todo.component";
   template: `
       <h1>Liste de chose à faire</h1>
 
-      <a href="#" role="button" (click)="onClickTodo()">A faire</a>
-      <a href="#" role="button" (click)="onClickTodoCompleted()">Terminée</a>
-      <a href="#" role="button" (click)="onClickTodoShowAll()">Affciher tout</a>
+      <a href="#" role="button" (click)="onClickTodo()" [class.secondary]="!completedFiler && !showAll">A faire</a>
+      <a href="#" role="button" (click)="onClickTodoCompleted()" [class.secondary]="completedFiler && !showAll">Terminée</a>
+      <a href="#" role="button" (click)="onClickTodoShowAll()" [class.secondary]="showAll">Afficher tout</a>
       <ng-container *ngFor="let todo of todoList">
-          <app-todo [value]="todo" [listTodo]="todoList" *ngIf="(todo.isCompleted == completedFiler) || (showAll)"></app-todo>
+          <app-todo [value]="todo" [listTodo]="todoList" *ngIf="(todo.isCompleted === completedFiler) || (showAll)"></app-todo>
       </ng-container>
   `,
   styles: [
